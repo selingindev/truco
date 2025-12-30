41707f3353580d2eea92b4a5f6e6e7e9c2f0f733
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-30 20:23:07 -0300

    começando jogador controller

--- truco/src/jogador/JogadorController.java
+++ truco/src/jogador/JogadorController.java
@@ -1,10 +1,25 @@
 package jogador;
 
+import java.util.List;
+
+import cartas.CartaModel;
+
 public class JogadorController {
     
-    
-
-    void jogarCarta(JogadorModel jogador, CartaModel carta) {
-        jogador.getMao()
+    public void jogarCarta(JogadorModel jogador, CartaModel carta) {
+        if(jogador.isYourTurn = false) return;
     }
+
+    public List<CartaModel> verMao(JogadorModel jogador) {
+        return jogador.getMao();
+    }
+
+    private void removerCarta(JogadorModel jogador, CartaModel carta){
+        List<CartaModel> maoAtual = jogador.getMao();
+        maoAtual.remove(carta);
+        jogador.setMao(maoAtual);
+    }
+
+
 }
+ 
---
+++ truco/src/rodada/Rodada.java
@@ -0,0 +1,13 @@
+package rodada;
+
+import java.util.List;
+
+import baralho.BaralhoController;
+import jogador.JogadorModel;
+
+public class Rodada {
+    List<JogadorModel> jogadores;
+    BaralhoController baralhoController;
+    
+     
+}
