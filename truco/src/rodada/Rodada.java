9221b29dd0dd03bce41d754e3afbd2aca8d0286f
selingindev <88356338+selingindev@users.noreply.github.com>
2026-01-08 22:04:49 -0300

    subidno restante

--- truco/src/rodada/Rodada.java
+++ truco/src/rodada/Rodada.java
@@ -1,12 +1,9 @@
 package rodada;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import baralho.BaralhoModel;
-import cartas.CartaModel;
 
 public class Rodada {
    
