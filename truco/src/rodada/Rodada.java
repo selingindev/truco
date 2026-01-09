576cc98520c6aebf4afbd9de61a80c2204303936
selingindev <88356338+selingindev@users.noreply.github.com>
2026-01-08 22:04:38 -0300

    rodada model

--- truco/src/rodada/Rodada.java
+++ truco/src/rodada/Rodada.java
@@ -1,20 +1,64 @@
 package rodada;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import baralho.BaralhoModel;
-import jogador.JogadorModel;
+import cartas.CartaModel;
 
 public class Rodada {
    
     BaralhoModel baralho;   
-     
+    Map<String, Integer> placar = new HashMap<>();
+    int pesoRodada = 1;
+    int pesoTruco = 3;
 
     
+    Rodada(BaralhoModel baralho){
+        this.baralho = baralho;
+    }
 
 
+    public BaralhoModel getBaralho() {
+        return baralho;
+    }
 
 
+    public void setBaralho(BaralhoModel baralho) {
+        this.baralho = baralho;
+    }
+
+
+    public Map<String, Integer> getPlacar() {
+        return placar;
+    }
+
+
+    public void setPlacar(Map<String, Integer> placar) {
+        this.placar = placar;
+    }
+
+
+    public int getPesoRodada() {
+        return pesoRodada;
+    }
+
+
+    public void setPesoRodada(int pesoRodada) {
+        this.pesoRodada = pesoRodada;
+    }
+
+
+    public int getPesoTruco() {
+        return pesoTruco;
+    }
+
+
+    public void setPesoTruco(int pesoTruco) {
+        this.pesoTruco = pesoTruco;
+    }
     
      
 }
