e0ad301b7460e0f6003986844ecf6474439ba4e3
selingindev <arthur_santos@outlook.com.br>
2026-07-23 09:30:39 -0300

    metodo separado

---
+++ truco/src/utils/Utils.java
@@ -0,0 +1,20 @@
+package utils;
+
+import rodada.EnumRodada.QuemVenceuEnum;
+
+import java.util.ArrayList;
+
+public class Utils {
+
+   public static int contadorDeIncidencia(ArrayList<QuemVenceuEnum> objeto, QuemVenceuEnum comparacao){
+        int contador = 0;
+        for(QuemVenceuEnum enuns : objeto){
+            if (enuns.equals(comparacao)){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+
+}
